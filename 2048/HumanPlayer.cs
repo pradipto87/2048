3d87aec30026157b44a4840d133743562173fb33
Pradipto Biswas <35199284+pradipto87@users.noreply.github.com>
2024-04-29 17:25:29 +0530

    Update HumanPlayer.cs
    init 2

--- 2048/HumanPlayer.cs
+++ 2048/HumanPlayer.cs
@@ -1,18 +1,36 @@
-﻿namespace _2048
+namespace _2048
 {
-	public class HumanPlayer : IPlayer
-	{
-		public HumanPlayer()
-		{
-		}
-		public string GetPlayerType()
-		{
-			return "Human";
-		}
-		public Direction GetMove(Game game)
-		{
-			return Direction.Up;
-		}
-	}
+    public class HumanPlayer : IPlayer
+    {
+        public HumanPlayer()
+        {
+        }
+        public string GetPlayerType()
+        {
+            return "Human";
+        }
+        public Direction GetMove(Game game)
+        {
+            System.ConsoleKeyInfo keyInfo = System.Console.ReadKey(true);
+            System.Console.SetCursorPosition(0, System.Console.CursorTop);
+            System.Console.Write(new string(' ', System.Console.WindowWidth));
+            switch (keyInfo.Key)
+            {
+                case System.ConsoleKey.UpArrow:
+                    return Direction.Up;
+                case System.ConsoleKey.LeftArrow:
+                    return Direction.Left;
+                case System.ConsoleKey.DownArrow:
+                    return Direction.Down;
+                case System.ConsoleKey.RightArrow:
+                    return Direction.Right;
+                case System.ConsoleKey.R:
+                    return Direction.Reset;
+                case System.ConsoleKey.Q:
+                    return Direction.Quit;
+                default:
+                    return GetMove(game);
+            }
+        }
+    }
 }
-
