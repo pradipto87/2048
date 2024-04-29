5b5f1b31575da91bd16843bb583672fc9b92499c
Pradipto Biswas <35199284+pradipto87@users.noreply.github.com>
2024-04-29 17:23:56 +0530

    Update Program.cs
    init 2

--- 2048/Program.cs
+++ 2048/Program.cs
@@ -1,16 +1,22 @@
-﻿namespace _2048
+namespace _2048
 {
-	class Program
-	{
-		static void Main()
-		{
-			Game game = new Game();
-			IPlayer player = new HumanPlayer();
-			while (!game.IsOver())
-			{
-				Direction move = player.GetMove(game);
-				game.Update(move);
-			}
-		}
-	}
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game();
+            IPlayer player = new HumanPlayer();
+            while (!game.IsOver())
+            {
+                game.Display();
+                Direction move = player.GetMove(game);
+                if (move == Direction.Quit)
+                    break;
+                else if (move == Direction.Reset)
+                    game = new Game();
+                else
+                    game.Update(move);
+            }
+        }
+    }
 }
